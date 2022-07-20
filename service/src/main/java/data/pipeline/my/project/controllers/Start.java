eda25e270ccbe61543f718599f63d0ec09726734
Lokost <Lokost39@yandex.ru>
2022-07-20 15:58:45 +0400

    fix docker-compose.yml

---
+++ service/src/main/java/data/pipeline/my/project/controllers/Start.java
@@ -0,0 +1,16 @@
+package data.pipeline.my.project.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@RestController
+public class Start {
+
+    @GetMapping
+    public Mono<String> start(){
+        return Mono.just(UUID.randomUUID().toString());
+    }
+}
