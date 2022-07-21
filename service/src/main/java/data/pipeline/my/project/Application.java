3c904e436b2716759620e7febf9cdd5cb0f8c88d
Lokost <Lokost39@yandex.ru>
2022-07-21 17:51:31 +0400

    fix docker-compose.yml

--- service/src/main/java/data/pipeline/my/project/Application.java
+++ service/src/main/java/data/pipeline/my/project/Application.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Swagger Demo", version = "1.0", description = "Documentation APIs v1.0"))
 @EnableWebFlux
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
--- service/src/main/java/data/pipeline/my/project/controllers/Start.java
+++ service/src/main/java/data/pipeline/my/project/controllers/Start.java
@@ -1,14 +1,26 @@
 package data.pipeline.my.project.controllers;
 
+import data.pipeline.my.project.services.kafka.SendMessageTask;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class Start implements StartApi {
+    @Autowired
+    private SendMessageTask sendMessageTask;
 
     public Mono<String> start(){
+        try {
+            sendMessageTask.send();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return Mono.just(UUID.randomUUID().toString());
     }
 }
